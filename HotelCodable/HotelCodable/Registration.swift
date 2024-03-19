4fcf7427da13d35d41aa9bd38c8588a8da92ba2f
Derek Stengel <derekstengel@Dereks-MacBook-Pro.local>
2024-03-19 10:16:34 -0600

    added hotelCodable

---
+++ HotelCodable/HotelCodable/Registration.swift
@@ -0,0 +1,22 @@
+//
+//  Registration.swift
+//  HotelCodable
+//
+//  Created by Derek Stengel on 3/18/24.
+//
+
+import Foundation
+
+struct Registration {
+    var firstName: String
+    var lastName: String
+    var emailAddress: String
+    
+    var checkInDate: Date
+    var checkOutDate: Date
+    var numberOfAdults: Int
+    var numberOfChildren: Int
+    
+    var wifi: Bool
+    var roomType: RoomType
+}
